8cea21731f7bc1d6a295ba2e525ce11cfc2513cc
DimkaVinka <dimkavinka@gmail.com>
2022-05-06 14:50:38 +0300

    made second screen design

--- SpaceX_Starships/MVVM/Modal Screen/ModalView.swift
+++ SpaceX_Starships/MVVM/Modal Screen/ModalView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct ModalView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .frame(width: .infinity, height: 120)
+                .foregroundColor(.init(uiColor: .systemGray5))
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("FalconSat")
+                        .font(.title2)
+                    Text("2 февраля, 2022")
+                        .foregroundColor(.gray)
+                }.padding(.init(top: 0, leading: 30, bottom: 0, trailing: 0))
+                Spacer()
+                Image("success")
+                    .resizable()
+                    .frame(width: 45, height: 45)
+                    .padding(.init(top: 0, leading: 0, bottom: 0, trailing: 30))
+            }
+        }.padding()
     }
 }
 
 struct ModalView_Previews: PreviewProvider {
     static var previews: some View {
         ModalView()
+            .preferredColorScheme(.dark)
     }
 }
