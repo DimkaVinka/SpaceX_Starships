dc8f4c785335cc3883dd68f4eee961289c4fc80c
DimkaVinka <dimkavinka@gmail.com>
2022-05-06 11:29:21 +0300

    Initial Commit

---
+++ SpaceX_Starships/SpaceX_StarshipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceX_StarshipsApp.swift
+//  SpaceX_Starships
+//
+//  Created by Дмитрий Виноградов on 06.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpaceX_StarshipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
